0df96489f6ce68d339ba6b5af67b3945acd329d6
rashmirekha99 <kothalawalarr.19@uom.lk>
2022-12-24 13:26:01 +0530

    Certificate Viewer List

---
+++ lib/component/list/certificate_viewer.dart
@@ -0,0 +1,48 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class CetificateViewer extends StatefulWidget {
+  const CetificateViewer({Key? key}) : super(key: key);
+
+  @override
+  State<CetificateViewer> createState() => _CetificateViewerState();
+}
+
+class _CetificateViewerState extends State<CetificateViewer> {
+  final List<String> certificatePaths = [
+    'asset/certificates/certificate-01.png',
+    'asset/certificates/certificate-02.png',
+    'asset/certificates/certificate-03.png',
+  ];
+  late Widget certificateViewer;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    List<Widget> imageChildren = [];
+    certificatePaths.reversed.toList().asMap().forEach((index, certificate) {
+      int angleDegree;
+      if (index == certificatePaths.length - 1) {
+        angleDegree = 0;
+      } else {
+        angleDegree = Random().nextInt(10) - 8;
+      }
+      imageChildren.add(Transform.rotate(
+        angle: angleDegree * (pi / 180),
+        child: Image.asset(
+          certificate,
+          alignment: Alignment.center,
+          fit: BoxFit.cover,
+        ),
+      ));
+    });
+    certificateViewer = Stack(children: imageChildren);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(padding: EdgeInsets.only(top: 20), child: certificateViewer);
+  }
+}
--- lib/screens/continue_watching_screen.dart
+++ lib/screens/continue_watching_screen.dart
@@ -1,10 +1,10 @@
 import 'dart:math';
-
 import 'package:demo/model/ExploreItems.dart';
 import 'package:demo/model/continueWatching.dart';
 import 'package:flutter/material.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
 import '../../model/continueWatching.dart';
+import '../component/list/certificate_viewer.dart';
 import '../component/list/continue_watching_list.dart';
 import '../constant.dart';
 import '../model/CardItems.dart';
@@ -58,50 +58,5 @@ class ContinueWatchingScreen extends StatelessWidget {
         ],
       ),
     );
-  }
-}
-
-class CetificateViewer extends StatefulWidget {
-  const CetificateViewer({Key? key}) : super(key: key);
-
-  @override
-  State<CetificateViewer> createState() => _CetificateViewerState();
-}
-
-class _CetificateViewerState extends State<CetificateViewer> {
-  final List<String> certificatePaths = [
-    'asset/certificates/certificate-01.png',
-    'asset/certificates/certificate-02.png',
-    'asset/certificates/certificate-03.png',
-  ];
-  late Widget certificateViewer;
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    List<Widget> imageChildren = [];
-    certificatePaths.reversed.toList().asMap().forEach((index, certificate) {
-      int angleDegree;
-      if (index == certificatePaths.length - 1) {
-        angleDegree = 0;
-      } else {
-        angleDegree = Random().nextInt(10) - 5;
-      }
-      imageChildren.add(Transform.rotate(
-        angle: angleDegree * (pi / 180),
-        child: Image.asset(
-          certificate,
-          alignment: Alignment.center,
-          fit: BoxFit.cover,
-        ),
-      ));
-    });
-    certificateViewer = Stack(children: imageChildren);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(padding: EdgeInsets.only(top: 20), child: certificateViewer);
   }
 }
